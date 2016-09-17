c11779af74c5c92055b8af529a84e3ff6b08e9e0
Deniz Mekik <denizmekik@gmail.com>
2016-09-17 13:45:56 -0700

    new component

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,57 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var App = React.createClass({
+
+  render : function(){
+    return (
+      <div className="catch-of-the-day">
+      <div className="menu">
+        <Header />
+      </div>
+      <Order />
+      <Inventory />
+    </div>
+  )
+  }
+})
+
+var Header = React.createClass({
+  render : function() {
+    return (
+    <p>Header</p>
+    )
+  }
+})
+
+var Order = React.createClass({
+  render : function() {
+    return (
+    <p>Order</p>
+    )
+  }
+})
+
+var Inventory = React.createClass({
+  render : function() {
+    return (
+    <p>Inventory</p>
+    )
+  }
+})
+
 var StorePicker = React.createClass({
 
   render: function() {
     return (
-      <p>hello</p>
+      <form className="store-selector" >
+        <h2>Please Enter A Store</h2>
+        <input type="text" ref="storeID" />
+        <input type="submit" />
+      </form>
     )
   }
 });
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
 
